e1ed3edd799d8fc12012c763c71587075f11ee26
Jie <zhangjie.sysu@gmail.com>
2017-09-17 22:53:15 +1000

    Initial Commit

---
+++ fpingx/fpingx.h
@@ -0,0 +1,19 @@
+//
+//  fpingx.h
+//  fpingx
+//
+//  Created by Jie Zhang on 17/9/17.
+//  Copyright © 2017 Jie Zhang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for fpingx.
+FOUNDATION_EXPORT double fpingxVersionNumber;
+
+//! Project version string for fpingx.
+FOUNDATION_EXPORT const unsigned char fpingxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fpingx/PublicHeader.h>
+
+
